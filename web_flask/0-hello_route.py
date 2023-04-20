cbefa8d386c145742541ca65309c5f36b0ef8adc
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-20 11:28:13 +0100

    feat: add simple hello bnb server

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Setup a simple webserver with flask
+
+from flask import Flask, redirect, request
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+@app.before_request
+def clear_trailing():
+    rp = request.path
+    if rp != '/' and rp.endswith('/'):
+        return redirect(rp[:-1])
+
+@app.route('/')
+def helloBNB():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,3 @@
+# Flask
+This folder contains solutions to ALX SE tasks on seting a web appliation with
+flask, using it's builtin templating engine (jinja) and handling requests.
